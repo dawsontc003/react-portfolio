26ffff09dde0fe1db10cf3c8c117737433e4783b
Thomas Dawson <dawsontc003@gmail.com>
2021-02-25 23:08:15 -0700

    updated project

--- src/components/pages/Learn.js
+++ src/components/pages/Learn.js
@@ -4,6 +4,7 @@ function Learn() {
   return (
     <div>
       <h1 className="text-center">Notable Projects</h1>
+      <h5 class="card-title">Colorado Park Rangers</h5>
       <img
         src="https://dawsontc003.github.io/responsive-portfolio/Assets/parkrangers.PNG"
         class="card-img-to"
@@ -11,8 +12,7 @@ function Learn() {
         width="400"
         height="300"
       ></img>
-      <h5 class="card-title">Colorado Park Rangers</h5>
-
+      <br></br>
       <a
         id="project1repo"
         href="https://github.com/dawsontc003/CO_Park_Rangers"
@@ -22,6 +22,131 @@ function Learn() {
       <br></br>
       <a id="project1url" href="https://dawsontc003.github.io/CO_Park_Rangers/">
         https://dawsontc003.github.io/CO_Park_Rangers
+      </a>
+      <p>
+        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii
+        partiendo sed ne, no mutat ludus aperiri mea, per in choro dolorem
+        electram. Invidunt reprimique assueverit quo ne, eruditi graecis pro ut.
+        Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
+      </p>
+      <h5 class="card-title">Budget Tracker</h5>
+      <img
+        src="rs.PNG"
+        class="card-img-to"
+        alt="..."
+        width="400"
+        height="300"
+      ></img>
+      <br></br>
+      <a id="project1repo" href="https://github.com/dawsontc003/budget-tracker">
+        https://github.com/dawsontc003/budget-tracker
+      </a>
+      <br></br>
+      <a id="project1url" href="https://limitless-citadel-07996.herokuapp.com/">
+        https://limitless-citadel-07996.herokuapp.com/
+      </a>
+      <p>
+        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii
+        partiendo sed ne, no mutat ludus aperiri mea, per in choro dolorem
+        electram. Invidunt reprimique assueverit quo ne, eruditi graecis pro ut.
+        Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
+      </p>
+      <h5 class="card-title">Workout Tracker</h5>
+      <img
+        src="PNG"
+        class="card-img-to"
+        alt="..."
+        width="400"
+        height="300"
+      ></img>
+      <br></br>
+      <a
+        id="project1repo"
+        href="https://github.com/dawsontc003/workout-tracker"
+      >
+        https://github.com/dawsontc003/workout-tracker
+      </a>
+      <br></br>
+      <a id="project1url" href="https://cryptic-beyond-51250.herokuapp.com/">
+        https://cryptic-beyond-51250.herokuapp.com/
+      </a>
+      <p>
+        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii
+        partiendo sed ne, no mutat ludus aperiri mea, per in choro dolorem
+        electram. Invidunt reprimique assueverit quo ne, eruditi graecis pro ut.
+        Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
+      </p>
+      <h5 class="card-title">Discover Your Inner-Animal</h5>
+      <img
+        src="PNG"
+        class="card-img-to"
+        alt="..."
+        width="400"
+        height="300"
+      ></img>
+      <br></br>
+      <a
+        id="project1repo"
+        href="https://github.com/dawsontc003/discover-your-animal"
+      >
+        https://github.com/dawsontc003/discover-your-animal/
+      </a>
+      <br></br>
+      <a id="project1url" href="https://sleepy-sierra-89293.herokuapp.com/">
+        https://sleepy-sierra-89293.herokuapp.com/
+      </a>
+      <p>
+        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii
+        partiendo sed ne, no mutat ludus aperiri mea, per in choro dolorem
+        electram. Invidunt reprimique assueverit quo ne, eruditi graecis pro ut.
+        Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
+      </p>
+      <h5 class="card-title">Note Taker</h5>
+      <img
+        src="PNG"
+        class="card-img-to"
+        alt="..."
+        width="400"
+        height="300"
+      ></img>
+      <br></br>
+      <a id="project1repo" href="https://github.com/dawsontc003/note-taker">
+        https://github.com/dawsontc003/note-taker/
+      </a>
+      <br></br>
+      <a
+        id="project1url"
+        href="https://shielded-everglades-01019.herokuapp.com/"
+      >
+        https://shielded-everglades-01019.herokuapp.com/
+      </a>
+      <p>
+        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii
+        partiendo sed ne, no mutat ludus aperiri mea, per in choro dolorem
+        electram. Invidunt reprimique assueverit quo ne, eruditi graecis pro ut.
+        Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
+      </p>
+      <h5 class="card-title">Secure Password Generator</h5>
+      <img
+        src="PNG"
+        class="card-img-to"
+        alt="..."
+        width="400"
+        height="300"
+      ></img>
+      <br></br>
+      <a
+        id="project1repo"
+        href="https://github.com/dawsontc003/secure-password-generator"
+      >
+        https://github.com/dawsontc003/secure-password-generator/
+      </a>
+      <br></br>
+      <a
+        id="project1url"
+        href="https://dawsontc003.github.io/secure-password-generator/"
+      >
+        https://dawsontc003.github.io/secure-password-generator/
       </a>
       <p>
         Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii
