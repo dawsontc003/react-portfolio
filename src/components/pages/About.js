b4b5f19b40d9755142efb7ddecdd1cba6e35ede8
Thomas Dawson <dawsontc003@gmail.com>
2021-02-27 08:50:37 -0700

    updated about function for resume display

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,26 @@
-import React from "react";
-import { Document } from "react-pdf";
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf";
 
 function About() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages, pageNumber }) {
+    setNumPages(numPages);
+    setPageNumber(pageNumber);
+  }
+
   return (
     <div>
-      <Document file="https://dawsontc003.github.io/responsive-portfolio/Assets/Dawson,%20Thomas%20Resume.pdf" />
+      <Document
+        file="https://dawsontc003.github.io/responsive-portfolio/Assets/Dawson,%20Thomas%20Resume.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
     </div>
   );
 }
