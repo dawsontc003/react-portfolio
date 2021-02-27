78e756492e730b33b99973dbb3ee116f02b4ddf5
Thomas Dawson <dawsontc003@gmail.com>
2021-02-26 22:08:02 -0700

    updated about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,12 +4,75 @@ function About() {
   return (
     <div>
       <h1>About Page</h1>
+
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Thomas Dawson
+        <br></br>
+        Aurora, CO 720-490-1810 dawsontc003@gmail.com SUMMARY OF QUALIFICATIONS
+        • Software Development professional with 4 years of experience of
+        various software development roles that include Business Analyst,
+        Quality Analyst, Product Owner, and Product Support roles. • Strategic
+        thinker with the ability to lead, delegate, supervise tasks, and monitor
+        workflows. • Ability to collaborate and articulate business requirements
+        to meet project goals. • Outstanding communication skills to effectively
+        interacting with stake holders and leadership groups to provide
+        information on project status’. • Ability to efficient identify and
+        resolve issues to ensure quality teamwork • Technically proficient in
+        Word, Excel, PowerPoint, Trello, and Jira • Knowledgeable in HTML, CSS,
+        SQL, and JavaScript computer languages EDUCATION & CREDENTIALS
+        Certificate of completion Coding Bootcamp to be awarded March 2021
+        University of Denver College Denver, CO Leadership Program 2019 Urban
+        Leadership Foundation of Colorado, Denver, CO Kansas State University
+        2006-2007 Manhattan, KS Associates Degree in Business Administration
+        awarded through 2006 Trinidad State Junior College Trinidad, CO High
+        School Diploma awarded through 2003 Eaglecrest High School, Aurora, CO
+        WORK EXPERIENCE Connect for Health Colorado, Denver, CO 2019 – Present
+        LEAD Business Analyst • As team lead, I assist current Business Analysts
+        with story writing techniques and department policies and procedures to
+        maximize their skills and abilities to provide quality development user
+        stories. • First line of defense for Business Analysts questions that
+        surround existing implementation of functionality to increase bandwidth
+        of direct manager. • Identify process issues and promote a creative
+        environment by problem solving issues with team members, other
+        departments, and managers. • Communicate with external departments to
+        help gather business requirements and take those requirements and
+        establish the technical requirements to accomplish the business
+        requirements. • Communicate and escalate needs of the team to Managers,
+        as well as advise managers of behavioral issues from team members. •
+        Recognizes gaps in individual team members performance and strategize
+        ways to recognize and remove gaps. Connect for Health Colorado, Denver,
+        CO 2017 – 2019 Eligibility Business Analyst • Create and organize
+        technical development user stories. • Clearly communicate business and
+        functional requirements so that developers have a definite understanding
+        of desired outcomes. Thomas Dawson Aurora, CO 720-490-1810
+        dawsontc003@gmail.com • Developed and implemented best practices to
+        improve processes and efficiency, which contributed to achieving policy
+        compliance as well as process requirements. • Collaborated with
+        departments in the organization team to help them understand newly
+        developed application features. • Assisted with testing completed
+        development and documented validation of the completed test. • Analyzed
+        reported bugs both from production and non-production environments to
+        communicate issues back to the original development team. Connect for
+        Health Colorado, Denver, CO 2016 – 2017 Legislative Support Specialist •
+        Analyze reported issues that were considered on a high-profile issue.
+        Issues were considered high-profile based on where the issue was
+        initially reported, or they could be escalated. • Communicate outcome of
+        the analyzation of the reported issue while providing direction on the
+        path to resolution while documenting all proceeding up through the
+        resolution. • Document all customer contacts related to the reported
+        issue so all communication is transparent. • Notify and work with
+        different departments to expedite required resolutions. • Communicate
+        resolution to third parties initiating high-profile issues such as local
+        state legislative offices. Technical Experience Endurance International
+        Group Centennial, CO – Website development 2011 – 2012 Independent web
+        development, CO – Self-employed 2011 – Present • Managed development and
+        hosting of websites. • Collect and analyzed web traffic data to prepare
+        detailed reports summarizing user activities. • Identified short-term
+        and long-term goals based on user activity. • Collaborated with business
+        owners on marketing ideas and strategies. • Used advanced communication
+        and interpersonal skills to provide support and understanding of design
+        and technical implementation. OTHER EXPERIENCE Project Management 2018 –
+        Present Business Operations Consultant 2016 – Present
       </p>
     </div>
   );
--- src/components/pages/Learn.js
+++ src/components/pages/Learn.js
@@ -24,16 +24,17 @@ function Learn() {
         https://dawsontc003.github.io/CO_Park_Rangers
       </a>
       <p>
-        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii
-        partiendo sed ne, no mutat ludus aperiri mea, per in choro dolorem
-        electram. Invidunt reprimique assueverit quo ne, eruditi graecis pro ut.
-        Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
+        This is an informational application. This application can be utilized
+        for any one who is interested in National Parks in the State of
+        Colorado. This application will provide you with not only the location
+        and hours of the national parks but it also will provide a list of
+        activities that are available at each park.
       </p>
       <h5 class="card-title">Budget Tracker</h5>
       <img
-        src="rs.PNG"
+        src="https://dawsontc003.github.io/responsive-portfolio/Assets/budget-tracker.GIF"
         class="card-img-to"
-        alt="..."
+        alt="image of budget tracker project"
         width="400"
         height="300"
       ></img>
@@ -46,16 +47,18 @@ function Learn() {
         https://limitless-citadel-07996.herokuapp.com/
       </a>
       <p>
-        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii
-        partiendo sed ne, no mutat ludus aperiri mea, per in choro dolorem
-        electram. Invidunt reprimique assueverit quo ne, eruditi graecis pro ut.
-        Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
+        This application is all about documenting transactions. This application
+        provides a ledger where both revenue and expenses can be tracked. The
+        real power of this application comes into play when a user is offline.
+        One can not only still access the application but also input tranactions
+        and soon as the application is back online any submitted transactions
+        will be sent to the database.
       </p>
       <h5 class="card-title">Workout Tracker</h5>
       <img
-        src="PNG"
+        src="https://dawsontc003.github.io/responsive-portfolio/Assets/workout-tracker.GIF"
         class="card-img-to"
-        alt="..."
+        alt="image of workout tracker project"
         width="400"
         height="300"
       ></img>
@@ -71,16 +74,16 @@ function Learn() {
         https://cryptic-beyond-51250.herokuapp.com/
       </a>
       <p>
-        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii
-        partiendo sed ne, no mutat ludus aperiri mea, per in choro dolorem
-        electram. Invidunt reprimique assueverit quo ne, eruditi graecis pro ut.
-        Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
+        This application is a simple workout tracker. Users can enter a given
+        excercise and not only track that workout but record the workout and
+        pick up from a stopping point and continue. The application will track
+        reps as well as time.
       </p>
       <h5 class="card-title">Discover Your Inner-Animal</h5>
       <img
-        src="PNG"
+        src="https://dawsontc003.github.io/responsive-portfolio/Assets/inner-animal.GIF"
         class="card-img-to"
-        alt="..."
+        alt="image of inner animal project"
         width="400"
         height="300"
       ></img>
@@ -96,16 +99,18 @@ function Learn() {
         https://sleepy-sierra-89293.herokuapp.com/
       </a>
       <p>
-        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii
-        partiendo sed ne, no mutat ludus aperiri mea, per in choro dolorem
-        electram. Invidunt reprimique assueverit quo ne, eruditi graecis pro ut.
-        Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
+        The Inner-Animal application is a fun website that allows a user to
+        answer a series of personality questions. Once the questions are
+        submitted the application will evaluate the users responses and compare
+        the evaluation outcome to a set scores table. Once compared to the
+        scores table the user will be presente with an animal image that matches
+        their personality.
       </p>
       <h5 class="card-title">Note Taker</h5>
       <img
-        src="PNG"
+        src="https://dawsontc003.github.io/responsive-portfolio/Assets/note-taker.GIF"
         class="card-img-to"
-        alt="..."
+        alt="image of note taker project"
         width="400"
         height="300"
       ></img>
@@ -121,16 +126,16 @@ function Learn() {
         https://shielded-everglades-01019.herokuapp.com/
       </a>
       <p>
-        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii
-        partiendo sed ne, no mutat ludus aperiri mea, per in choro dolorem
-        electram. Invidunt reprimique assueverit quo ne, eruditi graecis pro ut.
-        Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
+        This is an application that anyone can find useful. This Note Taker
+        application allows a user to record a desired note or comment along with
+        a title or category for easy recognition. Users can enter notes to be
+        saved to a database and update any notes as needed.
       </p>
       <h5 class="card-title">Secure Password Generator</h5>
       <img
-        src="PNG"
+        src="https://dawsontc003.github.io/responsive-portfolio/Assets/password-generator.GIF"
         class="card-img-to"
-        alt="..."
+        alt="image of secure password generator project"
         width="400"
         height="300"
       ></img>
@@ -149,10 +154,12 @@ function Learn() {
         https://dawsontc003.github.io/secure-password-generator/
       </a>
       <p>
-        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii
-        partiendo sed ne, no mutat ludus aperiri mea, per in choro dolorem
-        electram. Invidunt reprimique assueverit quo ne, eruditi graecis pro ut.
-        Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
+        The Secured Password generator website is useful to users who understand
+        the value of a secure password. This application takes desired inputs
+        from a user such as character length and character type to generate a
+        random sucure password. This will ensure passwords are not easily
+        discovered based on personal knowledge of which typically allows
+        passwords to be hacked.
       </p>
     </div>
   );
